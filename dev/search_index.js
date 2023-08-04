40f458468a606b9c05b95874afa5cec593656aaa
Documenter.jl <documenter@juliadocs.github.io>
2023-08-04 21:12:45 +0000

    build based on 6741266

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EquivariantModels","category":"page"},{"location":"#EquivariantModels","page":"Home","title":"EquivariantModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EquivariantModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EquivariantModels]","category":"page"}]
+}
