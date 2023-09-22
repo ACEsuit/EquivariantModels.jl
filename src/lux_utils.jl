f9a6ff433fc028e2e05bae6b6575ebc127e3c8dc
cheukhinhojerry <cheukhinhojerry@gmail.com>
2023-09-22 16:09:51 -0700

    another fix

--- src/lux_utils.jl
+++ src/lux_utils.jl
@@ -3,9 +3,5 @@ using LuxCore: AbstractExplicitLayer
 
 export concat_chain, append_layer
 
-# overiding the iterate method of Chain
-Base.iterate(chain::Chain, state = 1) = state > length(chain) ? nothing : chain[keys(chain)[state]]
-
-# TODO: can we do it inplaces or it is not needed anyways?
 append_layer(chain1::Chain, layer::AbstractExplicitLayer; l_name = Symbol("layer$(length(chain1) + 1)")) = Chain(; (; zip(keys(chain1), values(chain1))...)..., (; zip((l_name,), (layer, ))...)...)
 concat_chain(chain1::Chain, chain2::Chain) = Chain(; (; zip(keys(chain1), values(chain1))...)..., (; zip(keys(chain2), keys(chain2))...)...)
