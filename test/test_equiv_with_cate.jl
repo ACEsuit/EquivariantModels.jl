f8f7fd7a4f16ab607c44786bc2a702f3ffb8bd0e
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2023-09-24 16:53:29 -0700

    Re-enable tests for equivariant_cat_included_chain

--- test/test_equiv_with_cate.jl
+++ test/test_equiv_with_cate.jl
@@ -1,6 +1,7 @@
 using Polynomials4ML, StaticArrays, EquivariantModels, Test, Rotations, LinearAlgebra
 using ACEbase.Testing: print_tf
 using EquivariantModels: getspec1idx, _invmap, dropnames, SList, val2i, xx2AA, degord2spec
+using Combinatorics: permutations
 
 include("wigner.jl")
 
@@ -10,34 +11,41 @@ Aspec, AAspec = degord2spec(; totaldegree = 4,
                                   order = 2, 
                                   Lmax = 0, )
 cats = [:O,:C]
-
-ext(x,cats) = [ (x[i]..., s = cats) for i = 1:length(x)]
-AAspec_tmp = [ ext.(AAspec,cats[1])..., ext.(AAspec,cats[2])... ] |> sort
+cats_ext = [(:O,:C),(:C,:O),(:O,:O),(:C,:C)] |> unique
+AAspec_tmp = []
+for i = 1:length(AAspec)
+   push!(AAspec_tmp, [ (spec..., s = cats_ext[1]) for spec in AAspec[i] ])
+   push!(AAspec_tmp, [ (spec..., s = cats_ext[2]) for spec in AAspec[i] ])
+end
 pos = findall(x -> length(x)>1, AAspec)
-_AAspec_tmp = [ [(AAspec[i][1]..., s = cats[1]), (AAspec[i][2]..., s = cats[2])] for i in pos ]
-_AAspec_tmp2 = [ [(AAspec[i][1]..., s = cats[2]), (AAspec[i][2]..., s = cats[1])] for i in pos ]
+_AAspec_tmp = [ [(AAspec[i][1]..., s = cats_ext[1]), (AAspec[i][2]..., s = cats_ext[2])] for i in pos ]
+_AAspec_tmp2 = [ [(AAspec[i][1]..., s = cats_ext[2]), (AAspec[i][2]..., s = cats_ext[1])] for i in pos ]
 append!(AAspec_tmp,_AAspec_tmp)
 append!(AAspec_tmp,_AAspec_tmp2)
 
-luxchain, ps, st = equivariant_model(AAspec_tmp, L; categories=cats)
+luxchain, ps, st = equivariant_model(AAspec_tmp, L; categories=cats_ext)
 F(X) = luxchain(X, ps, st)[1]
+species = [ rand(cats) for i = 1:10 ]
+Species = [ (species[1], species[i]) for i = 1:10 ]
 
 @info("Testing the equivariance of chains that contain categorical basis")
 for ntest = 1:10
    local X, θ1, θ2, θ3, Q, QX
    X = [ @SVector(rand(3)) for i in 1:10 ]
+   XX = (X, Species)
    θ1 = rand() * 2pi
    θ2 = rand() * 2pi
    θ3 = rand() * 2pi
    Q = RotXYZ(θ1, θ2, θ3)
    QX = [SVector{3}(x) for x in Ref(Q) .* X]
+   QXX = (QX, Species)
    
-   print_tf(@test F(X)[1] ≈ F(QX)[1])
+   print_tf(@test F(XX)[1] ≈ F(QXX)[1])
 
    for l = 2:L
       D = wigner_D(l-1,Matrix(Q))'
       # D = wignerD(l-1, 0, 0, θ)
-      print_tf(@test norm.(Ref(D') .* F(X)[l] - F(QX)[l]) |> norm <1e-12)
+      print_tf(@test Ref(D') .* F(XX)[l] ≈ F(QXX)[l])
    end
 end
 
