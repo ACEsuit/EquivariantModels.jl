349201a435d713478db3d8837a158cbd7a5e86a1
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2023-10-01 11:36:45 -0700

    Moving those Radial_basis relevant code to a new .jl file

--- src/utils.jl
+++ src/utils.jl
@@ -1,40 +1,4 @@
-using Polynomials4ML: natural_indices, ScalarPoly4MLBasis, lux
-using LuxCore: AbstractExplicitContainerLayer
-
-struct Radial_basis
-   Rnl::AbstractExplicitContainerLayer
-   Radialspec::Vector{NamedTuple}
-end
-
-Radial_basis(Rnl::AbstractExplicitContainerLayer, spec_Rnl::Union{Vector{Int}, UnitRange{Int64}}) = 
-         Radial_basis(Rnl, [(n = i, ) for i in spec_Rnl])
-
-Radial_basis(Rnl::AbstractExplicitContainerLayer) = 
-         try 
-            Radial_basis(Rnl,natural_indices(Rnl.basis)) 
-         catch 
-            try 
-               Radial_basis(Rnl,natural_indices(Rnl.layers.poly.basis)) 
-            catch
-               error("The specification of this Radial_basis should be given explicitly!")
-            end
-         end
-
-# it is in its current form just for the purpose of testing
-function simple_radial_basis(basis::ScalarPoly4MLBasis,f_cut::Function=identity,f_trans::Function=identity; spec = nothing)
-   if isnothing(spec)
-      try 
-         spec = natural_indices(basis)
-      catch 
-         error("The specification of this Radial_basis should be given explicitly!")
-      end
-   end
-   
-   f(r) = f_cut(r) * f_trans(r)
-   
-   return Radial_basis(Chain(trans = WrappedFunction(xx -> [f(norm(x)) for x in xx]), 
-               poly = lux(basis), ), spec)
-end
+using Polynomials4ML: natural_indices
 
 """
 _invmap(a::AbstractVector)
@@ -58,7 +22,7 @@ function dropnames(namedtuple::NamedTuple, names::Tuple{Vararg{Symbol}})
 end
 
 """
-getspec1idx(spec1, Radial_basis, bYlm)
+getspec1idx(spec1, spec_Rnl, bYlm)
 Return a vector of tuples of indices of spec1 w.r.t actual indices (i.e. 1, 2, 3, ...) of bRnl and bYlm
 """
 function getspec1idx(spec1, spec_Rnl, bYlm)
